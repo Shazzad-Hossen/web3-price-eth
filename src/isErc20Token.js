b33af0a3f6e00c973fd4cd41a2a0c5b6c69b8282
Shazzad-Hossen <tipsfire.com@gmail.com>
2024-08-03 02:27:40 +0600

    changes is isErc20Token function done

--- src/isErc20Token.js
+++ src/isErc20Token.js
@@ -5,7 +5,7 @@ const isErc20Token = async(tokenAddress,PROVIDER)=>{
   try {
     const TOKEN_CONTRACT= new Contract(tokenAddress,ERC20_CONTRACT_ABI,PROVIDER.HTTP);
     await TOKEN_CONTRACT.decimals();
-    await TOKEN_CONTRACT.getBalance('0x2812037C3c77a9BcE4e7D6e767524CdaB7Cf3675');
+    await TOKEN_CONTRACT.balanceOf('0x2812037C3c77a9BcE4e7D6e767524CdaB7Cf3675');
     return {success: true, data:{isERC20Token: true}}
   } catch (error) {
     return {success: true, data:{isERC20Token: false}}
