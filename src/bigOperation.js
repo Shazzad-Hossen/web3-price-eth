46ca7573bcc4b0429aed7644cdd6274379c6fc0f
Shazzad-Hossen-Coredevs <shazzad_hossen@coredevs.ltd>
2024-08-08 15:31:12 +0600

    Big js operation added

--- src/bigOperation.js
+++ src/bigOperation.js
@@ -1,16 +1,16 @@
 const Big = require('big.js');
 const bigOperation={
     add: (num1,num2)=>{
-       return Big(num1).add(Big(num2)).toString();
+       return Big(num1).add(Big(num2)).toPrecision(24).toString();
     },
     sub: (num1,num2)=>{
-        return Big(num1).sub(Big(num2)).toString();
+        return Big(num1).sub(Big(num2)).toPrecision(24).toString();
      },
      mul: (num1,num2)=>{
-        return Big(num1).mul(Big(num2)).toString();
+        return Big(num1).mul(Big(num2)).toPrecision(24).toString();
      },
      div: (num1,num2)=>{
-        return Big(num1).div(Big(num2)).toString();
+        return Big(num1).div(Big(num2)).toPrecision(24).toString();
      },
 }
 module.exports=bigOperation;
